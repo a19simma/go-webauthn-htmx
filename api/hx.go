ce9dee795c4399b137bd1080a9f54d772e04ceb1
Simon Malm <simon.v.p.malm@gmail.com>
2023-09-24 18:32:42 +0200

    feat(toast): create a dynamic toast component

--- api/hx.go
+++ api/hx.go
@@ -1,10 +1,26 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"net/url"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func RegisterHXRoutes(hx fiber.Router) {
 	hx.Post("/test", func(c *fiber.Ctx) error {
-		_, err := c.WriteString("Hello!")
+		q, err := url.ParseQuery(string(c.Body()))
+		if err != nil {
+			return err
+		}
+		_, err = c.WriteString(q.Get("text"))
 		return err
+	})
+	hx.Post("/toast", func(c *fiber.Ctx) error {
+		q, err := url.ParseQuery(string(c.Body()))
+		if err != nil {
+			return err
+		}
+		return c.Render("components/toast", q.Get("text"))
+
 	})
 }
--- public/app.js
+++ public/app.js
@@ -1,1 +1,4 @@
 console.log("Hello World")
+const toastclick = (el) => {
+  el.outerHTML = "<div id='toast'></div>";
+}
